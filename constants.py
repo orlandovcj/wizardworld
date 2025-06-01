4d33a09ca9a4413f2f769d80131b188ac9161608
Orlando Castro <69274971+orlandovcj@users.noreply.github.com>
2025-06-01 15:25:18 -0300

    Passwords, key and hidden variables.

---
+++ constants.py
@@ -0,0 +1,6 @@
+# Passwords, keys, and variables that should remain hidden.
+
+API_KEY = "Your Telegram ChatBot API KEY"
+PRIVATE_KEY_ESABG = "WAX primary key of the wax wallet to mind and send NFTs" #
+PRIVATE_KEY_BRSKETCHART1 = "WAX primary key of the wax contract to send tokens" #
+DBACCESS = "MySQL_database_password"
